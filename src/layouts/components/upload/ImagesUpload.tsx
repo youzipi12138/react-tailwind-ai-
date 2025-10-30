2e5ad84cd9f78ec4f463d2eb16331df4a56896bc
youzipi12138 <ranren875@gmail.com>
2025-10-30 19:47:41 +0800

    fix： 修改上传文件时，如果没有点击到上传文件，就不能上传的bug

--- src/layouts/components/upload/ImagesUpload.tsx
+++ src/layouts/components/upload/ImagesUpload.tsx
@@ -3,7 +3,25 @@ import { Icon } from '@lobehub/ui';
 import { Folder, Upload as UploadIcon } from 'lucide-react';
 import type { MenuProps } from 'antd';
 import { Upload, Dropdown } from 'antd';
+import { css, cx } from 'antd-style';
 import { useImages } from '@/hooks/useImages';
+
+// dropdown menu item是relative布局的，所以需要使用css来设置 是定位区域
+// .ant-dropdown-menu-item {
+//   position: relative;
+//   display: flex
+// ;
+//   align-items: center;
+// }
+
+const hotArea = css`
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: transparent;
+  }
+`;
 
 const ImagesUpload: React.FC = () => {
   const { uploadImage } = useImages();
@@ -27,7 +45,7 @@ const ImagesUpload: React.FC = () => {
           }}
           showUploadList={false}
         >
-          <div className='text-myTexthighlight'>上传图片</div>
+          <div className={cx(hotArea, 'text-myTexthighlight')}>上传图片</div>
         </Upload>
       ),
       icon: <Icon icon={UploadIcon} />,
@@ -42,7 +60,9 @@ const ImagesUpload: React.FC = () => {
           }}
           showUploadList={false}
         >
-          <div className='text-myTexthighlight'>上传图片文件夹</div>
+          <div className={cx(hotArea, 'text-myTexthighlight')}>
+            上传图片文件夹
+          </div>
         </Upload>
       ),
       icon: <Icon icon={Folder} />,
@@ -51,7 +71,7 @@ const ImagesUpload: React.FC = () => {
   return (
     <div>
       {' '}
-      <Dropdown menu={{ items }} placement='bottomLeft'>
+      <Dropdown menu={{ items }} placement='bottomLeft' trigger={['click']}>
         <div className='border-mycolorlogo text-myTexthighlight ml-4 cursor-pointer rounded-sm border px-2 py-1'>
           上传图片
         </div>
